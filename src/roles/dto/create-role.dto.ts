e03d11bc85aa7f8c0e954a1b63b0f2c787fab5c0
boytrend0108 <boytrend@gmail.com>
2024-05-11 12:40:24 +0300

    add:pipe

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,4 +1,8 @@
+import { IsNumber, IsString } from "class-validator";
+
 export class CreateRoleDTO {
+  @IsString({ message: "value shoulb be a string" })
   readonly value: string;
+  @IsNumber({}, { message: "value should be a number" })
   readonly description: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsEmail, Length } from "class-validator";
 
 export class CreateUserDto {
   @ApiProperty({ example: 'еmail@test.com', description: 'Email address' })
+  @IsString({ message: 'Should be string' })
+  @IsEmail({}, { message: 'Invalid email message' })
   readonly email: string;
 
   @ApiProperty({ example: 'sdf64sdfnoiLk', description: 'User password' })
+  @IsString({ message: 'Should be string' })
+  @Length(4, 6, { message: "4- 16 chars" })
   readonly password: string;
 }
